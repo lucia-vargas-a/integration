061bed9cbdd0753daba352281e5524f6e997ac14
Lucia <lucia.vargas@marleyspoon.com>
2018-09-28 20:19:38 +0200

    mapper

---
+++ intro_to_hadoop_and_map_reduce/python/mapper.py
@@ -0,0 +1,31 @@
+import os
+import io
+
+def mapper(lines):
+	key = None
+	newKey = None
+	newValue = 0
+	value = 0
+	details = None
+
+	for line in sys.stdin:
+		data = line.strip().split("\t")
+
+		if data.lenght != 3:
+			print("wrong input: %s",data)
+		else:
+			newkey,newvalue,details = data
+			print (data)
+
+			if 	key == newKey:
+				value = value+newValue
+			else:
+				print (key,value)
+				key = newKey
+
+	def main():
+		lines = """Lucia\t20\t"Bella"\t"""
+		mapper(lines)
+
+
+
